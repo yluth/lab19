3e22d09ea99521d66431ad66ba8fe5b5c0f33183
John Sanchez <johnlsanchez@gmail.com>
2020-04-16 12:25:06 -0400

    added types

--- aTMcomponents.ml
+++ aTMcomponents.ml
@@ -1,7 +1,20 @@
-
 module ATMcomponents =
   struct
+    type id = int
+    type action =
+      | Balance           (* balance inquiry *)
+      | Withdraw of int   (* withdraw an amount *)
+      | Deposit of int    (* deposit an amount *)
+      | Next              (* finish this customer and move on to the next one *)
+      | Finished          (* shut down the ATM and exit entirely *)
     
+    
+    type account_spec = {name : string; id : id; balance : int}
+
+    
+
+
+
 
 
   end
